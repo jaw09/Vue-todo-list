31ade7ae081d62e0298e3b4acbcd1f020e9bcf5d
jiananWang <a0918666589@gmail.com>
2018-06-09 15:20:09 +0800

    git init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {}
+};
+</script>
+
+<style lang="scss">
+body {
+  font-family: "Roboto", sans-serif;
+  background-color: #e1e1e1;
+}
+
+.completed {
+  text-decoration: line-through;
+}
+</style>
