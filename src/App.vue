89c13add7f94fbf6b1efe26028824906d1a8f604
jiananWang <a0918666589@gmail.com>
2018-06-10 01:41:15 +0800

    add navbar component and components' structure

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,59 @@
 <template>
   <div id="app">
-
+    <todo-navbar @changeTab="changeTab"></todo-navbar>
+    <div class="container">
+      <div v-for="item in list" :key="item.id" v-if="currentTab === 'tasks' || currentTab === item.status">
+        {{item.title}}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar";
+
 export default {
   name: "App",
-  components: {}
+  data() {
+    return {
+      list: [
+        {
+          id: 1,
+          title: "第一事件",
+          timestamp: 1528557635,
+          comment: "",
+          status: "progress",
+          markUp: false,
+          file: ""
+        },
+        {
+          id: 2,
+          title: "第二事件",
+          timestamp: 1528559635,
+          comment: "",
+          status: "completed",
+          markUp: false,
+          file: ""
+        }
+      ],
+      currentTab: "tasks"
+    };
+  },
+  methods: {
+    changeTab(tab) {
+      const vm = this;
+      if (tab === "My Tasks") {
+        this.currentTab = "tasks";
+      } else if (tab === "Completed") {
+        this.currentTab = "completed";
+      } else {
+        this.currentTab = "progress";
+      }
+    }
+  },
+  components: {
+    TodoNavbar: Navbar
+  }
 };
 </script>
 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,72 @@
+<template>
+  <header class="bg-primary">
+    <div class="container">
+      <div class="row">
+        <div class="col-4 text-center">
+          <a href="#" class="cnav-item"  :class="{ active: tab === 'My Tasks'}" @click.prevent="changeTab">My Tasks
+            <i class="line"></i>
+          </a>
+        </div>
+        <div class="col-4 text-center">
+          <a href="#" class="cnav-item" :class="{ active: tab === 'In Progress'}" @click.prevent="changeTab">In Progress
+            <i class="line"></i>
+          </a>
+        </div>
+        <div class="col-4 text-center">
+          <a href="#" class="cnav-item" :class="{ active: tab === 'Completed'}" @click.prevent="changeTab">Completed
+            <i class="line"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tab: "My Tasks"
+    };
+  },
+  methods: {
+    changeTab(e) {
+      const vm = this;
+      vm.tab = e.target.innerText;
+      this.$emit("changeTab", vm.tab);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+header {
+  height: 76px;
+}
+
+.cnav-item {
+  position: relative;
+  display: block;
+  padding: 20px 0;
+  color: #00408b;
+  font-size: 24px;
+  transition: all 0.3s;
+  &:hover,
+  &:focus,
+  &.active {
+    color: white;
+    text-decoration: none;
+    outline: 0;
+    .line {
+      position: absolute;
+      left: 25%;
+      bottom: 0;
+      width: 50%;
+      height: 5px;
+      background-color: #00408b;
+    }
+  }
+}
+</style>
+
+
